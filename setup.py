3ebc87c53f399c0417a900b0a955e4f17839962d
jonathan vanasco <jonathan@2xlp.com>
2014-07-17 16:43:45 -0400

    v0.0.4

--- setup.py
+++ setup.py
@@ -11,29 +11,29 @@ README = README.split("\n\n", 1)[0] + "\n"
 
 requires = [
     "pyramid",
-    ]
+]
 
-setup(name="pyramid_subscribers_cookiexfer",
-      version="0.0.3",
-      description="transfers cookies from request to response on exceptions",
-      long_description=README,
-      classifiers=[
+setup(
+    name="pyramid_subscribers_cookiexfer",
+    version="0.0.4",
+    description="transfers cookies from request to response on exceptions",
+    long_description=README,
+    classifiers=[
         "Intended Audience :: Developers",
         "Framework :: Pyramid",
         "Programming Language :: Python",
         "License :: OSI Approved :: MIT License",
-        ],
-      keywords="web pyramid",
-      py_modules=['pyramid_subscribers_cookiexfer'],
-      author="Jonathan Vanasco",
-      author_email="jonathan@findmeon.com",
-      url="https://github.com/jvanasco/pyramid_subscribers_cookiexfer",
-      license="MIT",
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      tests_require = requires,
-      install_requires = requires,
-      test_suite='tests',
-      )
-
+    ],
+    keywords="web pyramid",
+    py_modules=['pyramid_subscribers_cookiexfer'],
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/pyramid_subscribers_cookiexfer",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    tests_require = requires,
+    install_requires = requires,
+    test_suite='tests',
+)
--- tests.py
+++ tests.py
@@ -6,15 +6,15 @@ import unittest
 # pyramid testing requirements
 from pyramid import testing
 
+
 class TestPyramid(unittest.TestCase):
 
     def setUp(self):
         self.config = testing.setUp()
-        #self.config.testing_add_subscriber('pyramid_subscribers_cookiexfer.new_request',event_iface='pyramid.events.NewRequest')
-        #self.config.testing_add_subscriber('pyramid_subscribers_cookiexfer.new_response',event_iface='pyramid.events.NewResponse')
+        # self.config.testing_add_subscriber('pyramid_subscribers_cookiexfer.new_request',event_iface='pyramid.events.NewRequest')
+        # self.config.testing_add_subscriber('pyramid_subscribers_cookiexfer.new_response',event_iface='pyramid.events.NewResponse')
         self.context = testing.DummyResource()
         self.request = testing.DummyRequest()
-
 
     def tearDown(self):
         testing.tearDown()
